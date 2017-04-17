0063603ec2b6160988c05cdc53870e8270504c3e
oliktva <oliktva@gmail.com>
2017-04-17 21:30:08 +0300

    add load.js

---
+++ js/load.js
@@ -0,0 +1,61 @@
+'use strict';
+
+window.load = (function () {
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  var getMessage = function () {
+    switch (xhr.status) {
+      case 403:
+        return 'Сервер нашел, что искал ты, но не покажет.';
+      case 404:
+        return 'Не найдено то, что ищешь ты.';
+      case 500:
+        return 'Не отвечает сервер, позже прийди ты.';
+      default:
+        return 'Ошибка неопознанная, подскажет к решению путь администратор.';
+    }
+  };
+
+  var createErrorElement = function () {
+    var error = document.createElement('div');
+    var message = document.createElement('div');
+    var btnWrapper = document.createElement('div');
+    var btn = document.createElement('a');
+
+    btn.textContent = 'Ну и ладно';
+    btn.classList.add('error-btn');
+
+    btnWrapper.classList.add('error-btn-wrapper');
+    btnWrapper.appendChild(btn);
+    message.textContent = getMessage();
+
+    error.classList.add('error-window');
+    error.appendChild(message);
+    error.appendChild(btnWrapper);
+    document.body.appendChild(error);
+
+    var setElementHidden = function (evt) {
+      evt.preventDefault();
+      error.classList.add('hidden');
+
+      btn.removeEventListener('click', setElementHidden);
+    };
+
+    btn.addEventListener('click', setElementHidden);
+  };
+
+  var load = function (url, onLoad) {
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        createErrorElement(xhr.status);
+      }
+    });
+    xhr.send();
+  };
+
+  return load;
+})();
--- js/map.js
+++ js/map.js
@@ -1,28 +1,34 @@
 'use strict';
 
 (function () {
-  /** @constant {number} */
-  var NUMBER_OF_PLACES = 8;
+  /** @constant {string} */
+  var URL = 'https://intensive-javascript-server-kjgvxfepjl.now.sh/keksobooking/data';
 
-  var places = window.data.createPlacesList(NUMBER_OF_PLACES);
-  window.data.sortPlacesByLocationY(places);
-  window.pin.renderPins(places);
-  window.showCard(places[0], window.pin.unsetActivePin);
+  /**
+   * @param  {Array<Object>} places
+   */
+  var addPinsToMap = function (places) {
+    window.data.sortPlacesByLocationY(places);
+    window.pin.renderPins(places);
+    window.showCard(places[0], window.pin.unsetActivePin);
 
-  var pins = window.pin.getPinsElements();
-  window.pin.setActivePin(pins[0]);
-  places.forEach(function (item, i) {
-    pins[i].addEventListener('click', function (evt) {
-      window.pin.setActivePin(evt.currentTarget);
-      window.showCard(item, window.pin.unsetActivePin);
-    });
-    pins[i].addEventListener('keydown', function (evt) {
-      if (window.checkKey.isEnter(evt)) {
+    var pins = window.pin.getPinsElements();
+    window.pin.setActivePin(pins[0]);
+    places.forEach(function (item, i) {
+      pins[i].addEventListener('click', function (evt) {
         window.pin.setActivePin(evt.currentTarget);
         window.showCard(item, window.pin.unsetActivePin);
-      }
+      });
+      pins[i].addEventListener('keydown', function (evt) {
+        if (window.checkKey.isEnter(evt)) {
+          window.pin.setActivePin(evt.currentTarget);
+          window.showCard(item, window.pin.unsetActivePin);
+        }
+      });
     });
-  });
+  };
+
+  window.load(URL, addPinsToMap);
   window.card.setCloseClickEvent();
   window.mainPin.setDraggable();
 })();
